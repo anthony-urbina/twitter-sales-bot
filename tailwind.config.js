f24e151f018f1cf2c26e2ed2ed38c7b2a6cb971c
anthony-urbina <aurbinava002@gmail.com>
2023-12-14 18:37:06 -0800

    installed ngrok

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // purge: ["./css/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
